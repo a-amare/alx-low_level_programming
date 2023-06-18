03b13f3d22c6ffd947717fc6ebeb3bacd93e114c
abel amare <abelamare3022@gmail.com>
2023-06-18 06:38:26 -0700

    s2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,13 @@
 
 int main(void)
 {
-  int d1, d2;
-  for (d1 = 0; d1 <= 9; d1++)
+  int d1;
+  for (d1 = 0; d1 < 9; d1++)
     {
-      for (d2 = 0; d2 <= 9; d2++)
-	{
-	  putchar(d1 + '0');
-	  putchar(d2 + '0');
-	  putchar(',');
-	}
+      putchar(d1 + '0');
+
     }
+  putchar(9 + '0');
   putchar('\n');
 
   return (0);
