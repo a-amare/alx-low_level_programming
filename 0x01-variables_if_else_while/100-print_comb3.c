1f3f678f8bc70d7fd5efc40d4a7ed86686347ead
abel amare <abelamare3022@gmail.com>
2023-06-18 16:46:39 -0700

    struttin_bonus

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+/*
+ * 100-print_comb3.c
+ * Author: Me
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two single-digit numbers
+ * separated by commas.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9 ; j++)
+		{
+			if (i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if ((i != 8) && (j != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
