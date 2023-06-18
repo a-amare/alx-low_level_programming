35cd0b90b65cd28e52fc64b0c71ea564f6e7eec3
abel amare <abelamare3022@gmail.com>
2023-06-18 14:22:29 -0700

    mez23

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 	/* this is the main fun */
 	int d1;
 
-	for (d1 = 0; d1 <=9; d1++)
+	for (d1 = 0; d1 <= 9; d1++)
 	{
 		putchar(d1 + '0');
-		if (d1 !=9)
+		if (d1 !=9 )
 		{
 			putchar(',');
 			putchar(' ');
