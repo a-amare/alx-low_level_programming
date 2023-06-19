544ad6a6422e3b43bcf0d4504e358fc87a77add4
abel amare <abelamare3022@gmail.com>
2023-06-18 17:23:27 -0700

    beaut

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,16 +18,16 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = 0; j <= 9 ; j++)
+		for (j = 1; j <= 9 ; j++)
 		{
-			if (i != j)
+			if ((i != j) && (j > i))
 			{
 				putchar(i + '0');
 				putchar(j + '0');
 
-				if ((i != 8) && (j != 9))
+				if ((i != 8) || (j !=9))
 				{
 					putchar(',');
 					putchar(' ');
