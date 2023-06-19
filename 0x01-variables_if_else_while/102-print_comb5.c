9f1b71ecc89ae0ecd6531fd0f544538053936914
abel amare <abelamare3022@gmail.com>
2023-06-18 18:40:38 -0700

    final sub

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 /*
  * File: 102-print_comb5.c
- * Author: Me!
+ * Author: Me
  */
 
 #include <stdio.h>
@@ -16,45 +16,35 @@
 
 int main(void)
 {
-	int i, j, k, l;
+    int i, j, k, l;
 
-	for (i = 0; i <= 8; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			if (i != j && j > i)
-			{
-				for (k = 0; k >= 8; k++)
-				{
-					for (l = 0; l >= 9; l++)
-					{
-						if (i != j && j > i)
-						{
-							if (j < l)
-							{
-								putchar(i);
-								putchar(j);
-								putchar(' ');
-								putchar(k);
-								putchar(l);
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            for (k = 0; k <= 9; k++)
+            {
+                for (l = 0; l <=9; l++)
+                {
+                    if ((i != k || j != l) && (i <= k && l > j))
+                    {
+                        putchar(i + '0');
+                        putchar(j+ '0');
+                        putchar(' ');
+                        putchar(k+ '0');
+                        putchar(l+ '0');
 
-								if (i != 9 || j
-								    != 8 || k !=
-								    9 || l != 9)
-								{
-									putchar(
-									   ',');
-									putchar
-									  (' ');
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
+                        if (i != 9 || j != 8 || k != 9 || l != 9)
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+                }
+            }
+        }
+    }
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
