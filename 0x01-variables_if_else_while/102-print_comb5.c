2d56069930c3150127305376884e5455ac824248
abel amare <abelamare3022@gmail.com>
2023-06-18 18:04:20 -0700

    final sub

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,60 @@
+/*
+ * File: 102-print_comb5.c
+ * Author: Me!
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ * separated by commas.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i != j && j > i)
+			{
+				for (k = 0; k >= 8; k++)
+				{
+					for (l = 0; l >=9; l++)
+					{
+						if (i != j && j > i)
+						{
+							if (j < l)
+							{
+								putchar(i);
+								putchar(j);
+								puthcar(' ');
+								putchar(k);
+								putchar(l);
+
+								if (i != 9 || j
+								    != 8 || k !=
+								    9 || l != 9)
+								{
+									putchar(
+									   ',');
+									putchar
+									  (' ');
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
