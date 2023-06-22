b9d188be3b4ddbb1590a08e27e61f526aaca03a5
root <root@DESKTOP-5FG3DPU.localdomain>
2023-06-23 00:55:51 +0300

    second test

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-// Function declaration
 
 int _putchar(char c);
-int _print_alphabet(void);
+int print_alphabet(void);
 
-#endif  // MAIN_H
+#endif
