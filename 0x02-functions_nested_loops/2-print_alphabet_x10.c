354c6a1d9b996921002a6af3f817ea66b17113b1
root <root@DESKTOP-5FG3DPU.localdomain>
2023-06-23 01:52:13 +0300

    another one bites the dust!

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+/**
+ * Function: print_alphabet_x10
+ *
+ * return: void
+ *
+*/
+
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+
+	}
+}
