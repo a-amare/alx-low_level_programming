77c97e2d8bc2d28a6b7228d67c9bbd87195ca364
root <root@DESKTOP-5FG3DPU.localdomain>
2023-06-28 04:59:46 +0300

    finally!

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,13 +13,12 @@ void print_rev(char *s)
 {
 	int x = 0;
 
-	while (*s != '\0')
+	while (*(s + x) != '\0')
 	{
 		x++;
-		s++;
 	}
 
-	while (x >= 0)
+	while (x > 0)
 	{
 		_putchar(*(s + x));
 		x--;
