cce481ce5d2c25fb9ef4adaf8802235ebe288064
root <root@DESKTOP-5FG3DPU.localdomain>
2023-06-28 04:43:46 +0300

    probably an error

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order
+ *
+ *@s: pointer variable/ address
+ *
+ * Return: none
+ *
+ */
+
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (*s != '\0')
+	{
+		x++;
+		s++;
+	}
+
+	while (x >= 0)
+	{
+		_putchar(*(s + x));
+		x--;
+	}
+	_putchar('\n');
+}
