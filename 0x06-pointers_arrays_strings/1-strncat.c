aa2a17788fed49e483c30b7b2662f7cf34a8f26d
root <root@DESKTOP-5FG3DPU.localdomain>
2023-06-29 22:48:46 +0300

    1 problem formatting to go...

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,22 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int d_len = 0, s_len = 0;
+    int d_len = 0, s_len = 0, i;
 
     while (dest[d_len] != '\0')
     {
         d_len++;
     }
+
+    while (src[s_len] != '\0')
+    {
+        s_len++;
+    }
+
+    for (i = 0; i < n && i < s_len; i++)
+    {
+        dest[d_len + i] = src[i];
+    }
+
+    return (dest);
 }
