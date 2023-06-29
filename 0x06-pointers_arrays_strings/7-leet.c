fd1d67814083ba247fd4c00dbd0b2c1445f9ff84
root <root@DESKTOP-5FG3DPU.localdomain>
2023-06-30 00:14:46 +0300

    Mozart composed his music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * leet - switches letters with numbers
+ * 
+ * Returns: someting
+*/
+
+char *leet(char *x)
+{
+    while (*x != '\0')
+    {
+        if (*x == 'a' || *x == 'A')
+        {
+            *x = '4';
+        }
+        else if (*x == 'e' || *x == 'E')
+        {
+            *x = '3';
+        }
+        else if (*x == 'o' || *x == 'O')
+        {
+            *x = '0';
+        }
+        else if (*x == 't' || *x == 'T')
+        {
+            *x = '7';
+        }
+        else if (*x == 'l' || *x == 'L')
+        {
+            *x = '1';
+        }
+        x++;
+
+    }
+
+    return (x);
+}
