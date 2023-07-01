864b086ad1e36941d3f2ea87c8ab54e47f435650
abel amare <abelamare3022@gmail.com>
2023-06-30 19:34:02 -0700

    7Th

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,41 +1,37 @@
-#include "main.h"
-
 /**
- * leet - switches letters with numbers
- * 
- * Returns: someting
+ * leet - a function that encodes a string into 1337
+ *
+ * @s: string input
+ *
+ * Return: @s
 */
 
-char *leet(char *x)
+char *leet(char *s)
 {
-    char * f = x;
-    while (*x != '\0')
-    {
-        if (*x == 'a' || *x == 'A')
-        {
-            *x = '4';
-        }
-        else if (*x == 'e' || *x == 'E')
-        {
-            *x = '3';
-        }
-        else if (*x == 'o' || *x == 'O')
-        {
-            *x = '0';
-        }
-        else if (*x == 't' || *x == 'T')
-        {
-            *x = '7';
-        }
-        else if (*x == 'l' || *x == 'L')
-        {
-            *x = '1';
-        }
-        x++;
+	int i, c = 0;
+	int sl[] = {97, 101, 111, 116, 108};
+	int ul[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
 
-    }
-
-    x = f;
-
-    return (x);
+	/*iterate values in array s*/
+	while (s[c] != '\0')
+	{
+		/**
+		 * loop through array value five times
+		 * to check if the value is equal to
+		 * any small letter in array sl or
+		 * upper letter in array ul and if it
+		 * is replace it with the value in array n
+		*/
+		for (i = 0; i < 5; i++)
+		{
+			if (s[c] == sl[i] || s[c] == ul[i])
+			{
+				s[c] = n[i];
+				break;
+			}
+		}
+		c++;
+	}
+	return (s);
 }
