cc5ef758d273e522b8726e796d64cbbeb40ab165
final_jefe <abelamare3022@gmail.com>
2023-07-05 23:34:33 +0300

    rewrote 0 eventhough it was correct ;D

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,8 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-	}
-
-	_putchar('\n');
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
