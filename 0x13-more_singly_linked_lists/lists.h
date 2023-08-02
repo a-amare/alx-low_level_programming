7b6ab90ac0237d178fe22d9f82799a85817e8f67
abel <abelamare3022@gmail.com>
2023-08-03 01:08:01 +0300

    betty woes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,8 +9,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * @n: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
