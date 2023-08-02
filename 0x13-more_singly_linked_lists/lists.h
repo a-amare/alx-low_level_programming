5a78629481f60a54ca07efe2a54f243bc6671219
abel <abelamare3022@gmail.com>
2023-08-02 03:43:02 +0300

    0. Print list

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/******************2***************/
+size_t print_listint(const listint_t *h);
+
+#endif
