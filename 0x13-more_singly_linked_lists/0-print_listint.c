565b9fb7b1e243f12cc2c0e2e9d3f6b1e9c1cd62
abel <abelamare3022@gmail.com>
2023-08-02 03:47:46 +0300

    added description to function

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_listint - Prints all the elements of a linked list of integers.
+ * @h: Pointer to the head node of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
 
 size_t print_listint(const listint_t *h)
 {
