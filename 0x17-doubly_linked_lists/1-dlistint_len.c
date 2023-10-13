b2f8233574dcad4c733626111d70cb5e3d028f04
abel <abelamare3022@gmail.com>
2023-10-14 02:25:03 +0300

    b

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 
 /**
-* dlistint_len - length of list
-* @h: 
-*/
+ * dlistint_len - length of list
+ * @h: pointer
+ * Return: count
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
